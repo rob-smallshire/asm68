21b1ae7730cdbfe64f6833fdd9e8995124b3ce84
Robert Smallshire <robert@smallshire.org.uk>
2017-11-14 21:10:48 +0100

    Tightened up twos_complement tests based on Cosmic Ray findings.

--- tests/test_twiddle.py
+++ tests/test_twiddle.py
@@ -1,7 +1,5 @@
-import sys
 from hypothesis import given
-from hypothesis.strategies import integers
-from math import log, floor
+from hypothesis.strategies import integers, data
 
 from pytest import raises
 
@@ -12,15 +10,33 @@ def test_twos_complement_non_positive_num_bits_raises_value_error(num_bits):
     with raises(ValueError):
         twos_complement(0, num_bits)
 
-@given(n=integers(min_value=1))
-def test_twos_complement_positive(n):
-    assert twos_complement(n, 2 + floor(log(n)/log(2))) == n
+@given(data())
+def test_twos_complement_positive(d):
+    num_bits = d.draw(integers(min_value=1))
+    value = d.draw(integers(min_value=0, max_value=(2**(num_bits-1)) - 1))
+    assert twos_complement(value, num_bits) == value
 
-@given(n=integers(min_value=1))
-def test_twos_complement_too_narrow(n):
+
+@given(d=data())
+def test_twos_complement_negative(d):
+    num_bits = d.draw(integers(min_value=1))
+    value = d.draw(integers(min_value=-2**(num_bits-1), max_value=-1))
+    c = int(''.join(str(int(not int(x))) for x in bin(abs(value) - 1)[2:]).rjust(num_bits, '1'), 2)
+    assert twos_complement(value, num_bits) == c
+
+
+@given(d=data())
+def test_twos_complement_positive_too_narrow(d):
+    value = d.draw(integers(min_value=0))
+    num_bits = d.draw(integers(max_value=value.bit_length() - 1))
     with raises(ValueError):
-        assert twos_complement(n, 1)
+        twos_complement(value, num_bits)
 
-@given(n=integers(min_value=-128, max_value=-1))
-def test_twos_complement_negative(n):
-    assert twos_complement(n, 8) == 256 + n
+
+@given(d=data())
+def test_twos_complement_negative_too_narrow(d):
+    value = d.draw(integers(max_value=-1))
+    num_bits = d.draw(integers(max_value=value.bit_length() - 1))
+    with raises(ValueError):
+        twos_complement(value, num_bits)
+
