d694659d25ac4095965fe6261608abe03f40a799
Robert Smallshire <rob@sixty-north.com>
2018-05-10 12:32:08 +0200

    A setuptools distribution program.

---
+++ setup.py
@@ -0,0 +1,80 @@
+import io
+import os
+
+from setuptools import setup, find_packages
+
+
+def local_file(*name):
+    return os.path.join(
+        os.path.dirname(__file__),
+        *name)
+
+
+def read(name, **kwargs):
+    with io.open(
+        name,
+        encoding=kwargs.get("encoding", "utf8")
+    ) as handle:
+        return handle.read()
+
+
+def read_version():
+    "Read the `(version-string, version-info)` from `asm68/version.py`."
+    version_file = local_file('asm68', 'version.py')
+    local_vars = {}
+    with open(version_file) as handle:
+        exec(handle.read(), {}, local_vars)
+    return (local_vars['__version__'], local_vars['__version_info__'])
+
+
+LONG_DESCRIPTION = read(local_file('README.rst'), mode='rt')
+
+
+INSTALL_REQUIRES = [
+    'frozendict',
+    'docopt',
+    'exit_codes',
+]
+
+setup(
+    name='asm68',
+    version=read_version()[0],
+    packages=find_packages(),
+
+    author='Robert Smallshire',
+    author_email='robert@smallshire.org.uk',
+    description='6809/6309 assembler as an internal Python Domain Specific Language',
+    license='MIT License',
+    keywords='asm assembly 6809 6309 8-bit',
+    url='http://github.com/rob-smallshire/asm68',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Assembly',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Assemblers',
+    ],
+    platforms='any',
+    include_package_data=True,
+    install_requires=INSTALL_REQUIRES,
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'test': ['hypothesis', 'pytest'],
+        'docs': ['sphinx', 'sphinx_rtd_theme'],
+    },
+    entry_points={
+        'console_scripts': [
+            'asm68 = asm68.cli:main',
+        ],
+    },
+    long_description=LONG_DESCRIPTION,
+)
+
