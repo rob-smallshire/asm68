2450ca1395d3eaa295e96ecc2e5cd291f03ac22f
Robert Smallshire <rob@sixty-north.com>
2020-12-19 21:36:34 +0100

    Renames Direct to PageDirect to be more descriptive

--- asm68/addrmodecodes.py
+++ asm68/addrmodecodes.py
@@ -1,6 +1,6 @@
 INH = "Inherent"           # The 'address' is inherent in the opcode. e.g. ABX
 IMM = "Immediate"          # Operand immediately follows the opcode. A literal. Could be 8-bit (LDA), 16-bit (LDD), or 32-bit (LDQ)
-DIR = "Direct"             # An 8-bit offset pointer from the base of the direct page, as defined by the DP register.
+DIR = "PageDirect"         # An 8-bit offset pointer from the base of the direct page, as defined by the DP register. Also known as just 'Direct'.
 IDX = "Indexed"            # Relative to the address in a base register (an index register or stack pointer)
 EXT = "Extended"           # A 16-bit pointer to a memory location
 REL8 = "Relative8 8-bit"   # Program counter relative
