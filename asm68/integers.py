72abc624d129ef65476037b53e1df9d75d269eae
Robert Smallshire <rob@sixty-north.com>
2020-12-20 22:04:44 +0100

    Apparently singledispatch doesn't work if you use the wrong imports

--- asm68/integers.py
+++ asm68/integers.py
@@ -1,73 +1,73 @@
-from util import typename
+from asm68.util import typename
 
 
 class U8:
-    
+
     def __init__(self, value):
         if not (0 <= value < 2**8):
             raise ValueError("{} value {!r} out of range".format(typename(self), value))
         self._value = value
-        
+
     @property
     def value(self):
         return self._value
 
 
 class U16:
-    
+
     def __init__(self, value):
         if not (0 <= value < 2**16):
             raise ValueError("{} value {!r} out of range".format(typename(self), value))
         self._value = value
-        
+
     @property
     def value(self):
         return self._value
 
 
 class U32:
-    
+
     def __init__(self, value):
         if not (0 <= value < 2**32):
             raise ValueError("{} value {!r} out of range".format(typename(self), value))
         self._value = value
-        
+
     @property
     def value(self):
         return self._value
-    
-    
+
+
 class I8:
-    
+
     def __init__(self, value):
         if not (-128 <= value < 127):
             raise ValueError("{} value {!r} out of range".format(typename(self), value))
         self._value = value
-        
+
     @property
     def value(self):
         return self._value
 
 
 class I16:
-    
+
     def __init__(self, value):
         if not (-32768 <= value < 32767):
             raise ValueError("{} value {!r} out of range".format(typename(self), value))
         self._value = value
-        
+
     @property
     def value(self):
         return self._value
 
 
 class I32:
-    
+
     def __init__(self, value):
         if not (-2147483648 <= value < 2147483647):
             raise ValueError("{} value {!r} out of range".format(typename(self), value))
         self._value = value
-        
+
     @property
     def value(self):
         return self._value
