fed04187c2a7dd0291cae6744e117f0e88bdd82c
Robert Smallshire <rob@sixty-north.com>
2020-12-20 22:05:05 +0100

    Code formatting

--- asm68/directives.py
+++ asm68/directives.py
@@ -5,15 +5,19 @@ from asm68.statement import Statement
 class Directive(Statement):
     pass
 
+
 class Org(Directive):
     mnemonic = ORG
 
+
 class Fcb(Directive):
     mnemonic = FCB
-    
+
+
 class Fdb(Directive):
     mnemonic = FDB
-    
+
+
 class Call(Directive):
     mnemonic = CALL
 
