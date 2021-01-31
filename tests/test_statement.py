cf407cea3567f6f023a59a249b5d1d52c4f9d5a9
Robert Smallshire <rob@sixty-north.com>
2021-01-31 20:31:05 +0100

    Adds tests for Statement to string conversions

--- tests/test_statement.py
+++ tests/test_statement.py
@@ -3,8 +3,10 @@ from hypothesis.strategies import composite, integers, text
 from pytest import raises
 
 from asm68.label import Label
-from asm68.statement import Statement
 from asm68.util import is_valid_variable_name
+from asm68.mnemonics import ABX
+from asm68.instructions import *
+from asm68.statement import Statement
 from tests.predicates import check_balanced
 from tests.test_label import label_names
 from tests.test_mnemonics import mnemonics
@@ -84,3 +86,15 @@ def test_label_repr(cls, operand, comment, label_name):
     assert repr(operand) in r
     assert repr(label) in r
     assert check_balanced(r)
+
+
+def test_statement_repr():
+    abx = Statement.from_mnemonic(ABX)
+    r = repr(abx)
+    assert r == 'AbX(operand=Inherent(), label=None)'
+
+
+def test_statement_str():
+    abx = Statement.from_mnemonic(ABX)
+    s = str(abx)
+    assert s == 'AbX(operand=Inherent(), label=None)'
