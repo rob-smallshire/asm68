060609539aa9f4fe1d421a0e5f599dbefbd1dfed
Robert Smallshire <rob@sixty-north.com>
2021-01-31 08:54:37 +0100

    Adds tests for the ADCA instruction

--- tests/test_instructions.py
+++ tests/test_instructions.py
@@ -1,6 +1,7 @@
 from asm68.asmdsl import AsmDsl, statements
 from asm68.assembler import assemble
-from asm68.mnemonics import ABX
+from asm68.registers import *
+from asm68.mnemonics import *
 
 
 def test_abx_inherent():
@@ -10,4 +11,37 @@ def test_abx_inherent():
     code = assemble(statements(asm))
     assert code[0] == bytes.fromhex(
         '3A'
+    )
+
+def test_adc_immediate():
+    asm = AsmDsl()
+    asm (ADCA, 0x34)
+    code = assemble(statements(asm))
+    assert code[0] == bytes.fromhex(
+        '89 34'
+    )
+
+
+def test_adc_direct():
+    asm = AsmDsl()
+    asm (ADCA, {0x34})
+    code = assemble(statements(asm))
+    assert code[0] == bytes.fromhex(
+        '99 34'
+    )
+
+def test_adc_indexed():
+    asm = AsmDsl()
+    asm (ADCA, {0:X})
+    code = assemble(statements(asm))
+    assert code[0] == bytes.fromhex(
+        'A9 84'
+    )
+
+def test_extended_direct():
+    asm = AsmDsl()
+    asm (ADCA, {0x1234})
+    code = assemble(statements(asm))
+    assert code[0] == bytes.fromhex(
+        'B9 12 34'
     )
